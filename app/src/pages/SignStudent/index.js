17e2e1b7b41272021283f74fe7f075f1451b80d0
Vinicius Souza <viisouza10@live.com>
2019-11-26 16:43:25 -0300

    ContainerStyled

--- app/src/pages/SignStudent/index.js
+++ app/src/pages/SignStudent/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Buttton from "../../components/Button";
+import { ContainerStyled } from "../../styles/components";
 import { TestStyled } from "./styles";
+
 export default function SignStudentPage({ navigation }) {
   return (
-    <>
+    <ContainerStyled>
       <TestStyled>SignStudent</TestStyled>
       <Buttton text="ola" />
-    </>
+    </ContainerStyled>
   );
 }
---
+++ app/src/styles/components.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const ContainerStyled = styled.div`
+  display: flex;
+  width: 80vw;
+  background-color: red;
+  margin: 0 auto;
+`;
