56cb6e39b1100909af28f24772b1eb2dd33aa727
Vinicius Souza <viisouza10@live.com>
2019-11-26 16:46:36 -0300

    center full height

--- app/src/styles/components.js
+++ app/src/styles/components.js
@@ -3,8 +3,9 @@ import styled from "styled-components";
 export const ContainerStyled = styled.div`
   display: flex;
   width: 80vw;
+  height: 100vh;
   background-color: red;
   margin: 0 auto;
   max-width: 1200px;
-  min-width: 1200px;
+  justify-content: center;
 `;
