3e3e87863a90c9df785a758c6677655e754432e1
Elzo Lourenço <elzoljr.91@gmail.com>
2019-11-27 00:24:42 -0300

    List Scool

--- app/src/pages/DashboardAdmin/index.js
+++ app/src/pages/DashboardAdmin/index.js
@@ -1,6 +1,56 @@
-import React from "react";
+import React,{useState} from "react";
 import { TestStyled } from "./styles";
+import emailIcon from '../../assets/e-mail-envelope.png';
+import api from "../../services/api";
+import { TitleLits,UlList, LiList, DivList,TextListBold,LabelList,TextList,DivRow,DivCol,DivCol20,DivAba} from "../../styles/components";
+export default function DashboardCompanyPage({ navigation }) {
+  const [schools,setSchools] = useState([]);
+  async function searchScool() {
+    // event.preventDefault();
+    const { data } = await api.get("/schools ");
+    setSchools(data);
+    
+  }
+  searchScool();
+  
 
-export default function DashboardAdminPage({ navigation }) {
-  return <TestStyled>DashboardAdmin</TestStyled>;
+
+  return (
+
+      <DivList>
+              <DivAba>
+        
+        </DivAba>
+           {/* <TestStyled>DashboardSchool</TestStyled> */}
+           <TitleLits>Escolas</TitleLits>
+           <UlList>
+           {schools.map(school =>
+             <LiList>
+               <DivRow>
+                  <DivCol>
+              
+                  <TextListBold>{school.name}</TextListBold>
+                  <TextList>{school.street}, {school.number} - {school.city}/ {school.state}</TextList>
+                  <TextList>{school.phone}</TextList>
+                  <TextList><a href="mailto:{school.mail}" >{school.email}</a></TextList>
+                 
+                  </DivCol>
+                
+                  <DivCol20>
+                    <LabelList>Alunos</LabelList>
+                    <TextListBold>23</TextListBold>
+                  </DivCol20>
+                
+                </DivRow>
+             </LiList>
+              )}
+           </UlList>
+
+
+      </DivList> 
+      // School:
+      // v
+   
+    );
 }
+  
