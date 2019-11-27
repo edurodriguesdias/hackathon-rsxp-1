279fdf21b266e14e0cced6537cd97ef80f267c5e
Eduardo <edu_rodriguesdias@hotmail.com>
2019-11-26 23:05:13 -0300

    school list

--- src/app/controllers/SchoolController.js
+++ src/app/controllers/SchoolController.js
@@ -1,10 +1,16 @@
 import School from '../models/School';
 
 class SchoolController {
+    async index(req, res) {
+        const schools = await School.findAll({
+            order: ['name'],
+        });
+
+        return res.json(schools);
+    }
+
     async store(req, res) {
         const school = await School.create(req.body);
-
-        
 
         return res.json(school);
     }
