00815bb2796fff0badcad322b5f788ec13a86b9e
Eduardo <edu_rodriguesdias@hotmail.com>
2019-11-27 01:49:45 -0300

    dev - init school api consume

--- src/app/controllers/SchoolController.js
+++ src/app/controllers/SchoolController.js
@@ -1,11 +1,12 @@
 import School from '../models/School';
+import axios from 'axios';
 
 class SchoolController {
     async index(req, res) {
         const schools = await School.findAll({
             order: ['name'],
         });
-
+        
         return res.json(schools);
     }
 
@@ -13,6 +14,39 @@ class SchoolController {
         const school = await School.create(req.body);
 
         return res.json(school);
+    }
+
+    async searchAvailableSchools(req, res) {
+
+        try {
+            const { data } = await axios.get('http://educacao.dadosabertosbr.com/api/escolas', {
+                params: {
+                    nome: 'josepha pinto chiavelli'
+                }
+            });
+
+            let schoolData = [];
+
+            if( data ) {
+                
+                schoolData = data[1].map( (schoolresp) => {
+
+                    if( schoolresp.situacaoFuncionamento == 1){
+                        return {
+                            schoolId:  schoolresp.cod,
+                            schoolName:  schoolresp.nome
+                        }
+                    }
+                });             
+            }
+
+        
+            return res.status(200).json({ data: schoolData });
+
+        } catch( err ) {
+            console.log(err)
+            return res.status(500).json({ error: err });
+        };
     }
 }
 
