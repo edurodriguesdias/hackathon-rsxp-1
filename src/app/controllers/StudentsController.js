c7f5e5e2e142bb586035ba37c96ecd6754d8faec
Vitor Breda <vitor@olivasdigital.com.br>
2019-11-27 10:35:49 -0300

    Liberando cors

--- src/app/controllers/StudentsController.js
+++ src/app/controllers/StudentsController.js
@@ -2,14 +2,13 @@ import Students from '../models/Student';
 
 class StudentsController {
     async store(req, res) {
-
         const hasStudentEmail = await Students.findOne({
             where: {
-                email: req.body.email
-            }
+                email: req.body.email,
+            },
         });
 
-        if ( hasStudentEmail ) { 
+        if (hasStudentEmail) {
             return res.status(400).json({ error: 'Student already exists.' });
         }
 
