90f3735f32701726b71ffd30bdadf68cf38d5e07
Vinicius Souza <viisouza10@live.com>
2019-11-27 00:16:37 -0300

    toast

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import Routes from './routes';
-import GlobalStyle from './styles/global';
+import React from "react";
+import "react-toastify/dist/ReactToastify.css";
+import Routes from "./routes";
+import GlobalStyle from "./styles/global";
 
-require('dotenv').config();
+require("dotenv").config();
 
 function App() {
-    return (
-        <>
-                <Routes />
-            <GlobalStyle />
-        </>
-    );
+  return (
+    <>
+      <Routes />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
