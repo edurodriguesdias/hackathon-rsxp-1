efccbd199f8e4b712cd4b3d0ccbeb6723b983dee
Vinicius Souza <viisouza10@live.com>
2019-11-26 22:08:03 -0300

    sign in school

--- app/src/pages/SignSchool/index.js
+++ app/src/pages/SignSchool/index.js
@@ -1,6 +1,28 @@
 import React from "react";
-import { TestStyled } from "./styles";
+import BackButton from "../../components/BackButton";
+import Button from "../../components/Button";
+import {
+  ContainerStyled,
+  FormStyled,
+  InputStyled
+} from "../../styles/components";
 
 export default function SignSchoolPage({ navigation }) {
-  return <TestStyled>SignSchool</TestStyled>;
+  return (
+    <ContainerStyled>
+      <BackButton />
+      <FormStyled>
+        <InputStyled type="text" placeholder="nome" />
+        <InputStyled type="tel" placeholder="telefone" />
+        <InputStyled type="mail" placeholder="e-mail" />
+        <InputStyled type="text" placeholder="endereço" />
+        <InputStyled type="tel" placeholder="numero" />
+        <InputStyled type="text" placeholder="bairro" />
+        <InputStyled type="text" placeholder="cidade" />
+        <InputStyled type="text" placeholder="uf" maxLength="2" />
+        <InputStyled type="tel" placeholder="cep" />
+        <Button text="cadastrar" />
+      </FormStyled>
+    </ContainerStyled>
+  );
 }
--- app/src/pages/SignStudent/index.js
+++ app/src/pages/SignStudent/index.js
@@ -1,13 +1,30 @@
 import React from "react";
+import BackButton from "../../components/BackButton";
 import Buttton from "../../components/Button";
-import { ContainerStyled } from "../../styles/components";
-import { TestStyled } from "./styles";
+import {
+  ContainerStyled,
+  FormStyled,
+  InputStyled
+} from "../../styles/components";
 
 export default function SignStudentPage({ navigation }) {
   return (
     <ContainerStyled>
-      <TestStyled>SignStudent</TestStyled>
-      <Buttton text="ola" />
+      <BackButton />
+      <FormStyled>
+        <InputStyled type="text" placeholder="nome" />
+        <InputStyled type="tel" placeholder="telefone" />
+        <InputStyled type="tel" placeholder="data de nascimento" />
+        <InputStyled type="mail" placeholder="e-mail" />
+        <InputStyled type="text" placeholder="endereço" />
+        <InputStyled type="tel" placeholder="numero" />
+        <InputStyled type="text" placeholder="bairro" />
+        <InputStyled type="text" placeholder="cidade" />
+        <InputStyled type="text" placeholder="uf" maxLength="2" />
+        <InputStyled type="tel" placeholder="cep" />
+        <InputStyled type="text" placeholder="escolaridade" />
+        <Buttton text="cadastrar" />
+      </FormStyled>
     </ContainerStyled>
   );
 }
