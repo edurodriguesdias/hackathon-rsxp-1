fbbc9036c3e26f10bccbb32107ba860b8f532c93
Eduardo <edu_rodriguesdias@hotmail.com>
2019-11-26 23:07:20 -0300

    change routes

--- src/routes.js
+++ src/routes.js
@@ -9,16 +9,17 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.post('/students', StudentsController.store);
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
+routes.post('/students', StudentsController.store);
 
 routes.post('/users', UserController.store);
 
 routes.get('/companies', CompanyController.index);
 routes.post('/companies', CompanyController.store);
 
+routes.get('/schools', SchoolController.index);
 routes.post('/schools', SchoolController.store);
 
 export default routes;
