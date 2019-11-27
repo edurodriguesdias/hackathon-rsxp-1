966a3450fb59a007990c72c9c4ab8d1f8044fa9f
Vitor Breda <vitor@olivasdigital.com.br>
2019-11-27 12:30:54 -0300

    Liberando rotas

--- src/routes.js
+++ src/routes.js
@@ -15,13 +15,13 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/sessions', SessionController.store);
+routes.get('/companies', CompanyController.index);
+routes.post('/students', StudentsController.store);
 
 routes.use(authMiddleware);
-routes.post('/students', StudentsController.store);
 
 routes.post('/users', UserController.store);
 
-routes.get('/companies', CompanyController.index);
 routes.post('/companies', CompanyController.store);
 
 routes.get('/schools', SchoolController.index);
